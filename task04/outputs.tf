b78cb8dd6c47025b9dbb2dead436d8387c66cf5d
priyanshu_kumar <priyanshu_kumar@epam.com>
2025-05-12 16:27:15 +0530

    first commit

---
+++ task04/outputs.tf
@@ -0,0 +1,9 @@
+output "vm_public_ip" {
+  description = "This is the public ip of the vm"
+  value = azurerm_public_ip.public_ip.ip_address
+}
+
+output "vm_fqdn" {
+  description = "this will show the domain name of the public ip of the vm"
+  value = azurerm_public_ip.public_ip.fqdn
+}
